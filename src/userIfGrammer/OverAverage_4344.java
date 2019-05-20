3e25adaaf9101f63c718d452985e11de6d6de1ea
jejecrunch <gkswlgp456@gmail.com>
2019-05-20 23:08:09 +0900

    190520 4344 평균은 넘겠지 살짝 변형했는데도 런타임에러 뜨고 난리.

--- src/userIfGrammer/OverAverage_4344.java
+++ src/userIfGrammer/OverAverage_4344.java
@@ -3,34 +3,32 @@ package userIfGrammer;
 import java.io.*;
 
 public class OverAverage_4344 {
-	
-	static double average(String[] arr, int num) {
-		double sum=0;
-		for(int i=1;i<=num;i++) {
-			sum+=Double.parseDouble(arr[i]);
-		}
-		return sum/num;
-	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int test_case = Integer.parseInt(bf.readLine());
-		String[][] r=new String[test_case][1000];
-		double[] avg=new double[test_case];
+		int n, total, count;
+		double avg;
+		int scores[] = new int[1000];
 		
 		for(int i=0;i<test_case;i++) {
-			r[i]=bf.readLine().split(" ");
-			avg[i]=average(r[i], Integer.parseInt(r[i][0]));
-		}
-		
-		for(int i=0;i<test_case;i++) {
-			int count=0;
-			for(int j=0;j<r[i].length;j++) {
-				if((Double.parseDouble(r[i][j])) > avg[i]) ++count;
+			n=Integer.parseInt(bf.readLine());
+			total=0;
+			count=0;
+			for(int j=0;j<n;j++) {
+				scores[j]=Integer.parseInt(bf.readLine());
+				total+=scores[j];
+			}
+			avg=(double)total/n;
+			
+			for(int j=0;j<n;j++) {
+				if(scores[j]>avg) count++;
 			}
 			
-			System.out.format("%.3f%%\n", (double)(count/Double.parseDouble(r[i][0])) * 100.0 );
+			System.out.printf("%.3f", 100.0*count/n);
+			System.out.println("%");
 		}
+		
 	}
 
 }
