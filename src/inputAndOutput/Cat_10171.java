bff664d0818b53dbc4971fd192a5a49cd862ccb4
jejecrunch <gkswlgp456@gmail.com>
2019-08-30 15:49:37 +0900

    10171 고양이 출력

---
+++ src/inputAndOutput/Cat_10171.java
@@ -0,0 +1,13 @@
+package inputAndOutput;
+
+public class Cat_10171 {
+
+	public static void main(String[] args) {
+		// 아래 예제와 같이 고양이를 출력하시오.
+		System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+	}
+
+}
