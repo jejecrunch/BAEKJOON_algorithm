2fd58dc191898dec377a0f1e1402c2970b227271
jejecrunch <gkswlgp456@gmail.com>
2019-09-10 18:38:47 +0900

    190910 1330 두 수 비교 한 줄에 입력받기 잊어서 다시 찾아봄 공부하자

---
+++ src/userIfGrammer/CompareTwoNum.java
@@ -0,0 +1,20 @@
+package userIfGrammer;
+
+import java.util.Scanner;
+
+public class CompareTwoNum {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a=scan.nextInt(), b=scan.nextInt();
+		
+		if(a>b)
+			System.out.println(">");
+		else if(a<b)
+			System.out.println("<");
+		else
+			System.out.println("==");
+	}
+
+}
