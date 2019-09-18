b2133ecadca58177a36f2e81795168195487492e
jejecrunch <gkswlgp456@gmail.com>
2019-09-18 16:00:31 +0900

    190918 11654 ASCIICode 문자를 아스키코드값으로 변환하는 것.

---
+++ src/useStrings/ASCIICode_11654.java
@@ -0,0 +1,15 @@
+package useStrings;
+import java.util.*;
+
+public class ASCIICode_11654 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		char[] input = s.next().toCharArray();
+		int ascii = (int) input[0];
+		
+		System.out.println(ascii);
+	}
+
+}
