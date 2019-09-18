fe75578267a830760221d60773108a63e486c4e0
jejecrunch <gkswlgp456@gmail.com>
2019-09-18 15:52:22 +0900

    190918 3052 Remainder 나머지는 배열의 인덱스를 활용할 것.

--- src/useOneDimensionalArray/Remainder_3052.java
+++ src/useOneDimensionalArray/Remainder_3052.java
@@ -5,18 +5,17 @@ public class Remainder_3052 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		ArrayList<Integer> inputs = new ArrayList<Integer>(10);
+		boolean[] arr = new boolean[42];
 		int count=0, input;
 		
 		for(int i=0;i<10;i++) {
 			input=s.nextInt(); // 입력받고
-			inputs.add(input%42); // 나머지 저장
+			if(!arr[input%42]) {
+				arr[input%42]=true;
+				++count;
+			}
 		}
 		
-		for(int i=1;i<inputs.size()-1;i++) {
-			if(inputs.subList(0, i-1).contains(inputs.get(i))||inputs.subList(i+1, inputs.size()-1).contains(inputs.get(i)))
-				++count;
-		}
 		
 		System.out.println(count);
 	}
