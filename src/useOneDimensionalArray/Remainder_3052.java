0db719711e0ba1659cdf41dc92859f40cc64011d
jejecrunch <gkswlgp456@gmail.com>
2019-09-18 15:48:06 +0900

    190918 3052 Remainder ArrayList 활용해서 재 시도.

--- src/useOneDimensionalArray/Remainder_3052.java
+++ src/useOneDimensionalArray/Remainder_3052.java
@@ -5,16 +5,17 @@ public class Remainder_3052 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int[] inputs = new int[10];
-		int count=1;
+		ArrayList<Integer> inputs = new ArrayList<Integer>(10);
+		int count=0, input;
 		
-		for(int i=0;i<inputs.length;i++) {
-			inputs[i]=s.nextInt(); // 입력받고
-			inputs[i]=inputs[i]%42; // 나머지 저장
+		for(int i=0;i<10;i++) {
+			input=s.nextInt(); // 입력받고
+			inputs.add(input%42); // 나머지 저장
 		}
 		
-		for(int i=1;i<inputs.length;i++) {
-			if(Arrays.asList(inputs).contains(inputs[i])) ++count;
+		for(int i=1;i<inputs.size()-1;i++) {
+			if(inputs.subList(0, i-1).contains(inputs.get(i))||inputs.subList(i+1, inputs.size()-1).contains(inputs.get(i)))
+				++count;
 		}
 		
 		System.out.println(count);
