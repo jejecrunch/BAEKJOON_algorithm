d72e14a40af33077d4801b9c601836e64661a8c8
jejecrunch <gkswlgp456@gmail.com>
2019-09-18 15:27:46 +0900

    190918 3052 Remainder List 이용해서 시도.

--- src/useOneDimensionalArray/Remainder_3052.java
+++ src/useOneDimensionalArray/Remainder_3052.java
@@ -6,7 +6,6 @@ public class Remainder_3052 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int[] inputs = new int[10];
-		List<int[]> list = Arrays.asList(inputs);
 		int count=1;
 		
 		for(int i=0;i<inputs.length;i++) {
@@ -14,8 +13,8 @@ public class Remainder_3052 {
 			inputs[i]=inputs[i]%42; // 나머지 저장
 		}
 		
-		for(int i=0;i<inputs.length;i++) {
-			if(list.contains(inputs[i])) count++;
+		for(int i=1;i<inputs.length;i++) {
+			if(Arrays.asList(inputs).contains(inputs[i])) ++count;
 		}
 		
 		System.out.println(count);
