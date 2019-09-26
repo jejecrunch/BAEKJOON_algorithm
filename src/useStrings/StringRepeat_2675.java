50f51aaaa7026fa4204ac44fdc1f029cba1e9894
jejecrunch <gkswlgp456@gmail.com>
2019-09-26 18:53:08 +0900

    190926 2675 StringRepeat 세번째로 안에 있는 for문의 k 증가값 조정을 잘 못함.

--- src/useStrings/StringRepeat_2675.java
+++ src/useStrings/StringRepeat_2675.java
@@ -5,19 +5,16 @@ public class StringRepeat_2675 {
 
 	public static void main(String[] args) {
 		Scanner c = new Scanner(System.in);
-		
 		int t = c.nextInt();
+		
 		for(int i=0;i<t;i++) {
 			int r = c.nextInt();
-			String s = c.nextLine();
+			String s = c.next();
 			
-			String result="";
-			for(int j=0;j<s.length();j++) {
-				for(int k=0;k<r;k=k++)
-					result += s.charAt(j);
-			}
-			result=result.replaceAll(" ", "");
-			System.out.println(result);
+			for(int j=0;j<s.length();j++)
+				for(int k=0;k<r;k++)
+					System.out.print(s.charAt(j));
+			System.out.println();
 		}
 		c.close();
 	}
