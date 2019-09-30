733a755f9372b50b42fd0a58ea455ad2f250e0db
jejecrunch <gkswlgp456@gmail.com>
2019-09-30 14:59:49 +0900

    190930 8958 OXquiz 왜 테스트를 5개를 받는데 왜 복사해도 그냥 쳐도 4개만 입력되는건지?

--- src/useOneDimensionalArray/OXquiz_8958.java
+++ src/useOneDimensionalArray/OXquiz_8958.java
@@ -5,18 +5,25 @@ public class OXquiz_8958 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-
 		int c = s.nextInt(); // 테스트 개수
-		int count, total;
-		
 		String[] test = new String[c]; 
-		
+
 		for(int i=0;i<c;i++) {
 			test[i]=s.nextLine();
 		}
-		
+		s.close();
+
+		int count, total;
 		for(String x : test) {
-			
+			count=0;
+			total=0;
+			for(int i=0;i<x.length();++i) {
+				if(x.charAt(i)=='O')
+					total+= ++count;
+				else
+					count = 0;
+			}
+			System.out.println(total);
 		}
 	}
 
