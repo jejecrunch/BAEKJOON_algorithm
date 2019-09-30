aeb584d74c930ef94c15572fb94c33e140456fd8
jejecrunch <gkswlgp456@gmail.com>
2019-09-30 15:59:23 +0900

    190930 2908 Sangsu 문자열 스텝이라 문자열로만 생각했는데 그냥 숫자로 받으면 될걸.

--- src/useStrings/Sangsu_2908.java
+++ src/useStrings/Sangsu_2908.java
@@ -6,21 +6,23 @@ public class Sangsu_2908 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		
-		String a=s.nextLine();
-		String[] temp=a.split(" ");
+		int a=s.nextInt(), b=s.nextInt();
 		
-		char tmp=temp[0].charAt(0);
-		temp[0]=temp[0].replace(temp[0].charAt(0), temp[0].charAt(2)).substring(0, 2);
-		temp[0]+=tmp;
+		a=reverse(a);
+		b=reverse(b);
 		
-		tmp=temp[1].charAt(0);
-		temp[1]=temp[1].replace(temp[1].charAt(0), temp[1].charAt(2)).substring(0, 2);
-		temp[1]+=tmp;
+		System.out.println(a>b?a:b);
+	}
+	
+	public static int reverse(int value) {
+		String result = "";
 		
-		if(Integer.parseInt(temp[0]) >= Integer.parseInt(temp[1])) {
-			System.out.println(temp[0]);
-		} else
-			System.out.println(temp[1]);
+		while(value != 0) {
+			result += (value%10);
+			value /= 10;
+		}
+		
+		return Integer.parseInt(result);
 	}
 
 }
