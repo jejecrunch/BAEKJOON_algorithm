e88667919f7fc04cfa18ff7f04ab6aa74a440bcc
jejecrunch <gkswlgp456@gmail.com>
2019-09-30 14:52:29 +0900

    190930 8958 OXquiz for문을 안나누고 하려니까 그지같아서 다시 하는 중

---
+++ src/useOneDimensionalArray/OXquiz_8958.java
@@ -0,0 +1,23 @@
+package useOneDimensionalArray;
+import java.util.*;
+
+public class OXquiz_8958 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int c = s.nextInt(); // 테스트 개수
+		int count, total;
+		
+		String[] test = new String[c]; 
+		
+		for(int i=0;i<c;i++) {
+			test[i]=s.nextLine();
+		}
+		
+		for(String x : test) {
+			
+		}
+	}
+
+}
