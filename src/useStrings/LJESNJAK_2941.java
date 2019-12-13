fa6305340adbba56ab555faf970c6101dd8cbe7b
jejecrunch <gkswlgp456@gmail.com>
2019-12-13 19:39:32 +0900

    191213 2941 LJESNJAK 알파벳 바꿔치기 해서 세기

--- src/useStrings/LJESNJAK_2941.java
+++ src/useStrings/LJESNJAK_2941.java
@@ -6,33 +6,14 @@ public class LJESNJAK_2941 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in); 
-		String input=scan.nextLine().trim();
-		scan.close();
-		char[] che = input.toCharArray();
-		int cnt=0;
+		String input=scan.next();
+		String[] crotia = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
 		
-		for(int i=0;i<input.length();i++) {
-			if(che[i]=='c'&&che[i+1]=='=') {
-				continue;
-			} else if (che[i]=='c'&&che[i+1]=='-') {
-				continue;
-			} else if (che[i]=='d'&&che[i+1]=='z'&&che[i+2]=='=') {
-				continue;
-			} else if (che[i]=='d'&&che[i+1]=='-') {
-				continue;
-			} else if (che[i]=='l'&&che[i+1]=='j') {
-				continue;
-			} else if (che[i]=='n'&&che[i+1]=='j') {
-				continue;
-			} else if (che[i]=='s'&&che[i+1]=='=') {
-				continue;
-			} else if (che[i]=='z'&&che[i+1]=='=') {
-				continue;
-			}
-			cnt++;
+		for(int i=0;i<crotia.length;i++) {
+			input = input.replace(crotia[i], "x");
 		}
 		
-		System.out.println(cnt);
+		System.out.println(input.length());
 	}
 
 }
