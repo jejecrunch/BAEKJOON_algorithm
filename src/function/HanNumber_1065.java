a0f3b197440759a4f71ea51e5a4dc9d7fc796ba7
jejecrunch <gkswlgp456@gmail.com>
2019-12-17 17:24:26 +0900

    191217 1065 HanNumber while 이용해서 자릿수 나누기. 등차수열이라는 개념을 갑자기 접하게 되니까 아는 개념도 사라져서 애 먹었음.

---
+++ src/function/HanNumber_1065.java
@@ -0,0 +1,32 @@
+package function;
+
+import java.util.Scanner;
+
+public class HanNumber_1065 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in); 
+		int n = scan.nextInt();
+		System.out.print(hanNum(n));
+	}
+
+	private static int hanNum(int input) {
+		int count=0, k;
+		int[] arr=new int[3];
+		for(int i=1;i<=input;i++) {
+			if(0 < i && i < 100) count=i;
+			else if(i==1000) break;
+			else {
+				k=2; int t=i;
+				while(t>0) {
+					arr[k]=t%10;
+					t/=10; k--;
+				}
+				if (arr[0]-arr[1]==arr[1]-arr[2]) count++;
+			}
+		}
+
+		return count;
+	}
+
+}
