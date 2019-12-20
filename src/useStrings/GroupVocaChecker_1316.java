def3133c823ec9e3b9cee64a8c005e35239dc39b
jejecrunch <gkswlgp456@gmail.com>
2019-12-20 20:39:07 +0900

    191220 1316 GroupVocaChecker 문제나 접근 방식은 쉽게 이해했는데 해답하는 과정에서 코드를 어떤 식으로 짜야할지 생각이 안나서 오래 걸렸던 문제. 나중에 다시 풀어보도록 하자.

--- src/useStrings/GroupVocaChecker_1316.java
+++ src/useStrings/GroupVocaChecker_1316.java
@@ -22,14 +22,20 @@ public class GroupVocaChecker_1316 {
 	private static boolean check(String input) {
 		boolean[] al = new boolean[26];
 		
+		char temp;
+		
 		for(int i=0;i<input.length();i++) {
-			char temp = input.charAt(i);
-			if(al[temp-'a']) {
-				return false;
-			} else {
-				if()
+			temp = input.charAt(i);
+			if(al[temp-'a']) // 이미 알파벳이 한번이라도 나온 적이 있는 경우
+				return false; // 그룹 단어가 아니다
+			else { // 알파벳이 한번이라도 나온 적이 없고
+				if(i<input.length()-1 && temp != input.charAt(i+1)) { // 글자 수보다 i가 작고, 다음 알파벳과 현재의 알파벳이 동일하지 않는 경우
+					al[temp-'a']=true; // 나온 알파벳이라는 것을 확인하기 위해 true 값을 대입
+				}
 			}
 		}
+		
+		return true;
 	}
 
 }
