9991bb5fd8122805cb15ce0c7b438979d1550117
jejecrunch <gkswlgp456@gmail.com>
2019-12-20 20:07:47 +0900

    191220 4673 SelfNumber 거꾸로 접근하는 방식을 이용. 없는 이라는 단어가 나오면 있는 걸 찾아서 ! 연산자를 이용해 있는 걸 뒤바꾸도록 하자.

--- src/function/SelfNumber_4673.java
+++ src/function/SelfNumber_4673.java
@@ -3,19 +3,20 @@ package function;
 public class SelfNumber_4673 {
 
 	public static void main(String[] args) {
-		boolean[] arr = new boolean[101];
-		for(int i=1;i<101;i++) {
-			arr[selfNumCheck(i)]=true;
+		boolean[] arr = new boolean[10001];
+		for(int i=1;i<10001;i++) {
+			if(selfNumCheck(i)<=10000)
+				arr[selfNumCheck(i)]=true;
 		}
 		
-		for(int i=1;i<101;i++) {
+		for(int i=1;i<10001;i++) {
 			if(!arr[i]) System.out.println(i);
 		}
 
 	}
 	
 	private static int selfNumCheck(int a) {
-		if(a/10==0) return a;
+		if(a/10==0) return a+a;
 		else if(a/100==0) return a+(a/10)+(a%10);
 		else if(a/1000==0) return a+(a/100)+(a%100/10)+(a%100%10);
 		else if(a/10000==0) return a+(a/1000)+(a%1000/100)+(a%1000%100/10)+(a%1000%100%10);
