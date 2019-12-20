ae17b862fa7cf76a62a0d2071ca128403d023364
jejecrunch <gkswlgp456@gmail.com>
2019-12-20 20:12:50 +0900

    191220 15596 TotalIntegers 제출 버튼을 눌렀더니 코드가 이미 짜여있는 게 있어서 기능 부분 코드만 추가함.

---
+++ src/function/TotalIntegers_15596.java
@@ -0,0 +1,16 @@
+package function;
+
+public class TotalIntegers_15596 {
+
+	public static void main(String[] args) {
+		
+	}
+
+	private static long sum(int[] a) {
+		long total=0;
+		for(int i=0;i<a.length;i++) {
+			total+=a[i];
+		}
+		return total;
+	}
+}
