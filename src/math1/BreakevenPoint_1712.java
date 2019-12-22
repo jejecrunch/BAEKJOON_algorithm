ee05f5f06f096083fcc83577a57a6d3c4693910f
jejecrunch <gkswlgp456@gmail.com>
2019-12-23 00:12:25 +0900

    191223 1712 BreakevenPoint 몫을 구하면 되는 문제. 생각보다 간단한 문제였다.

--- src/math1/BreakevenPoint_1712.java
+++ src/math1/BreakevenPoint_1712.java
@@ -6,17 +6,12 @@ public class BreakevenPoint_1712 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int a, b, c, result=-1, n=1;
+		long a, b, c, result=0, n=1;
 		
 		a=s.nextInt(); b=s.nextInt(); c=s.nextInt();
 		
-		while(true) {
-			if(a+(b*n)<c*n) {
-				result=n;
-				break;
-			}
-			n++;
-		}
+		if(c-b<=0) result=-1;
+		else result=(a/(c-b)+1);
 		
 		System.out.println(result);
 	}
