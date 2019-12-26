0ec37737d9a68ab81b9234da07336bddec9c8e42
jejecrunch <gkswlgp456@gmail.com>
2019-12-27 04:28:20 +0900

    191227 10250 ACMHotel 두번째 시도. 결과는 제대로 나오는데 수식이 이상한 듯. 더 쉽게 할 수 있을 것 같은데.

--- src/math1/ACMHotel_10250.java
+++ src/math1/ACMHotel_10250.java
@@ -8,24 +8,23 @@ public class ACMHotel_10250 {
 		Scanner s = new Scanner(System.in);
 		
 		int t=s.nextInt();
-		int stair=1, room, guest=0, result, count=1;
+		int stair, room, guest, count;
+		int[] result=new int[t];
 		
 		for(int i=0;i<t;i++)
 		{
+			count=1;
 			stair=s.nextInt();
 			room=s.nextInt();
 			guest=s.nextInt();
 			
-			int temp=guest;
-			
-			while(temp/stair!=0) {
-				temp-=stair;
-				count++;
-			}
-			
-			result=(guest%stair)*100+count;
-			
-			System.out.println(result);
+			if((guest%stair)==0)
+					result[i]=(guest/stair)*100+room;
+			else result[i]=(guest%stair)*100+((guest/stair)+1);
+		}
+		
+		for(int i=0;i<t;i++) {
+			System.out.println(result[i]);
 		}
 		
 	}
