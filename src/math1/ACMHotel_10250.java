a185a8dfddb891154228c145607fbc9a8e07b8e6
jejecrunch <gkswlgp456@gmail.com>
2019-12-27 04:34:47 +0900

    191227 10250 ACMHotel 수식은 맞았는데 if 문 안에 floor와 room의 수식이 헷갈림. 테스트 수 중 34 25 102를 넣으면 3403이 나와야 하는데 floor식과 room식이 서로 뒤바뀜.

--- src/math1/ACMHotel_10250.java
+++ src/math1/ACMHotel_10250.java
@@ -8,19 +8,21 @@ public class ACMHotel_10250 {
 		Scanner s = new Scanner(System.in);
 		
 		int t=s.nextInt();
-		int stair, room, guest, count;
 		int[] result=new int[t];
 		
 		for(int i=0;i<t;i++)
 		{
-			count=1;
-			stair=s.nextInt();
-			room=s.nextInt();
-			guest=s.nextInt();
+			int h=s.nextInt();
+			int w=s.nextInt();
+			int q=s.nextInt();
+			int floor=q%h, room=q/h+1;
 			
-			if((guest%stair)==0)
-					result[i]=(guest/stair)*100+room;
-			else result[i]=(guest%stair)*100+((guest/stair)+1);
+			if((q%h)==0){
+				floor=h;
+				room=q/h;
+			}
+			
+			result[i]=floor*100+room;
 		}
 		
 		for(int i=0;i<t;i++) {
