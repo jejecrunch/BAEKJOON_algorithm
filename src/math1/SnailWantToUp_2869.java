5d17705f9a4e3606c42a8ce304460f6eb2de29cf
jejecrunch <gkswlgp456@gmail.com>
2019-12-26 19:50:17 +0900

    191226 2869 SnailWantToUp 목표지점에 도달하면 미끄러지지 않으니까 -b 해주고 몫이 깔끔하게 나오려면 -1을 해주고 나중에 몫에 1을 더해주기. 왜냐면 일수를 구해야하니까.

--- src/math1/SnailWantToUp_2869.java
+++ src/math1/SnailWantToUp_2869.java
@@ -7,8 +7,8 @@ public class SnailWantToUp_2869 {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		
-		int a=s.nextInt(), b=s.nextInt(), c=s.nextInt();
-		int result = c/(a-b)-1;
+		int a=s.nextInt(), b=s.nextInt(), v=s.nextInt();
+		int result = (v-b-1)/(a-b) +1;
 
 		System.out.println(result);
 		
