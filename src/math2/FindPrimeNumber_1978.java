aaba40d3960eb934d5bdb2217775a7d2ffadb203
jejecrunch <gkswlgp456@gmail.com>
2020-01-01 23:39:14 +0900

    200101 1978 FindPrimeNumber j로 잘 바꿔놓고 왜 뒤에는 안 바꾸니.

--- src/math2/FindPrimeNumber_1978.java
+++ src/math2/FindPrimeNumber_1978.java
@@ -6,27 +6,28 @@ public class FindPrimeNumber_1978 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		
+
 		int n = s.nextInt();
 		int count=0;
-		
+		boolean flag=true;
+
 		for(int i=0;i<n;i++) {
 			int a=s.nextInt();
-			if(a!=1&&depart(n)>1) count++;
+
+			if(a<=1) flag=false;
+			else {
+				for(int j=2;j<=a;j++) {
+					if(a%j==0) { // 소수가 아니면
+						flag = false;
+						break;
+					}
+				}
+			}
+
+			if(flag) count++;
+			flag = true;
 		}
-		
+
 		System.out.println(count);
 	}
-	
-	private static int depart(int n) {
-		int result=0;
-		for(int i=2;i<=n;i++) {
-			while(n%i==0) {
-				n/=i;
-				result++;
-			}
-		}
-		return result;
-	}
-
 }
