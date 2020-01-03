dfa3c858069348bdef99d202119c2151220143e4
jejecrunch <gkswlgp456@gmail.com>
2020-01-03 19:46:18 +0900

    200103 1011 AlphaCentauri 문제 내용을 이해 못하겠어서 입력 틀만 만들고 포기. 다음에 다시 해보자.

---
+++ src/math1/AlphaCentauri_1011.java
@@ -0,0 +1,20 @@
+package math1;
+
+import java.util.*;
+
+public class AlphaCentauri_1011 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		int t = s.nextInt();
+		
+		for(int i=0;i<t;i++) {
+			int x = s.nextInt();
+			int y = s.nextInt();
+			
+			
+		}
+	}
+
+}
