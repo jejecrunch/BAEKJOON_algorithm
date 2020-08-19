51ce136d0bed8268087bf6dd38130e12b11f5c89
jejecrunch <gkswlgp456@gmail.com>
2020-08-20 03:01:18 +0900

    200820 11866 YosephuseQuestion0 현재 알고리즘은 생각이 났는데 그걸 어떻게 조건문으로 변환하면 좋을지 생각하는 중. 1~n까지 push하다가 k번째가 되면 pop을 하고 i가 n에 다다랐을 때 first에 있는 원소를 맨 끝으로 이동시키면 되는 것 같은데 조금 더 고민해보자.

---
+++ src/queue/YosephuseQuestion0_11866.java
@@ -0,0 +1,24 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class YosephuseQuestion0_11866 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Queue<Integer> que = new LinkedList<>();
+		
+		int n = scan.nextInt();
+		int k = scan.nextInt();
+
+		for(int i=1;i<=n;i++) {
+			que.add(i);
+			if() {
+				
+			}
+		}
+	}
+
+}
