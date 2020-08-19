78eadb44f951de736099da54d2a8ee401ccf00d9
jejecrunch <gkswlgp456@gmail.com>
2020-08-20 01:25:35 +0900

    200820 18258 Queue2 스캐너 써도 안되길래 좀 더 검색해본 결과 스캐너보다 버퍼드 라이터, 리더를 활용하는 것이 조금 더 빠를 수 있다는 얘기가 있어서 그것을 활용해봤다. 그리고 원래는 덱을 이용했었는데 덱을 이용하니까 덱 내 함수를 몰라서 활용을 못하겠어서 큐 클래스를 따로 만들었다. 이전 알고리즘 자료를 뒤적여서 성공.

--- src/queue/Queue2_18258.java
+++ src/queue/Queue2_18258.java
@@ -1,34 +1,101 @@
 package queue;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Queue2_18258 {
 
-	public static void main(String[] args) {
-		Deque<Integer> deq=new LinkedList();
-		Scanner scan = new Scanner(System.in); 
-		int size = scan.nextInt(); // 명령 수       
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int size = Integer.parseInt(br.readLine()); // 명령 수 
+		Queue que = new Queue(size); 
 
 		for(int i=0;i<size;i++) {
-			String inst = scan.next(); // 명령
-			if(inst.contains("push")) {
-				int value=scan.nextInt();
-				deq.offer(value);
-			}else if(inst.equals("pop")) {
-				System.out.println(deq.isEmpty()? -1 : deq.poll());
-			} else if(inst.equals("size")) {
-				System.out.println(deq.size());
-			} else if(inst.equals("empty")) {
-				System.out.println(deq.isEmpty()? 1:0);
-			} else if(inst.equals("front")) {
-				System.out.println(deq.isEmpty()? -1 : deq.getFirst());
-			} else if(inst.equals("back")) {
-				System.out.println(deq.isEmpty()? -1 : deq.getLast());
+			StringTokenizer st = new StringTokenizer(br.readLine(), " "); // 명령
+			String cmd = st.nextToken();
+
+			if(cmd.contains("push")) {
+				que.push(Integer.parseInt(st.nextToken()));
+			}else if(cmd.equals("pop")) {
+				que.pop();
+			} else if(cmd.equals("size")) {
+				que.size();
+			} else if(cmd.equals("empty")) {
+				que.empty();
+			} else if(cmd.equals("front")) {
+				que.front();
+			} else if(cmd.equals("back")) {
+				que.back();
 			}
 		}
 
+
+		que.bw.flush();
+	}
+}
+
+class Queue {
+	int[] queue;
+	int next=0;
+	int first=0;
+	BufferedWriter bw;
+
+	Queue(int n){
+		queue = new int[n];
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	}
+
+	void push(int n) {
+		queue[next]=n;
+		next++;
+	}
+
+	void pop() throws IOException {
+		if(next == first) {
+			bw.write("-1");
+		} else {
+			bw.write(queue[first]+"");
+			queue[first]=0;
+			first++;
+		}
+		bw.newLine();
+	}
+
+	void size() throws IOException {
+		bw.write(next-first+"");
+		bw.newLine();
+	}
+
+	void empty() throws IOException {
+		if(next == first) {
+			bw.write("1");
+		} else {
+			bw.write("0");
+		}
+		bw.newLine();
+	}
+
+	void front() throws IOException {
+		if(next == first) {
+			bw.write("-1");
+		} else {
+			bw.write(queue[first] + "");
+		}
+		bw.newLine();
+	}
+
+	void back() throws IOException {
+		if(next == first) {
+			bw.write("-1");
+		} else {
+			bw.write(queue[next-1] + "");
+		}
+		bw.newLine();
 	}
 
 }
